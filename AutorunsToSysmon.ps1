0318c88e66ecd06182c0376fdcf543c0b58e5fe9
dstaulcu <David_Staulcup@hotmail.com>
2016-11-30 20:00:48 -0500

    good enough version

--- AutorunsToSysmon.ps1
+++ AutorunsToSysmon.ps1
@@ -9,17 +9,48 @@
 $autoruns = 'c:\users\david\documents\development\projects\AutorunsToSysmon\autorunsc64.exe'
 $arguments = '-nobanner -a * -ct'
 $tmpfile = ($env:TEMP + '\autoruns_results.csv')
+$outfile = ($env:TEMP + '\sysmon_autoruns_rules.xml')
 
 # run autoruns and write stdout to CSV
-start-Process -FilePath $autoruns -ArgumentList $arguments -RedirectStandardOutput $tmpfile
+#start-Process -FilePath $autoruns -ArgumentList $arguments -RedirectStandardOutput $tmpfile
 
 # read CSV into object and dedup entry locations
 $entries = Import-Csv -Path $tmpfile -Delimiter "`t" | Select-Object -Property 'Entry Location' -Unique
 
-foreach ($entry in $entries) {
-    # further reduce list to only include registry type entries
-    if ($entry.'Entry Location' -like 'HKLM*' -or $entry.'Entry Location' -like 'HKCU*') {
-        # ok, here's we're we now start to transform.  going to bed now
-        Write-Host $entry.'Entry Location'
+
+$Keys = [System.Collections.ArrayList]@()
+
+if ($entries) {
+
+    foreach ($entry in $entries) {
+        # further reduce list to only include registry type entries
+        if ($entry.'Entry Location' -like 'HKLM*' -or $entry.'Entry Location' -like 'HKCU*') {
+
+            # strip off hklm\hkcu prefix to keys
+            $key = $entry.'Entry Location'
+            $key = $key -replace '^HKCU\\','\'
+            $key = $key -replace '^HKLM\\','\'
+
+            # only add unique values to an array
+            if ($keys.Contains($key) -eq $false) {
+                $keys.Add($key) | Out-Null
+            }
+        }
     }
+
+    #write the output to file
+    if (Get-Item($outfile)) {
+        Remove-Item -path $outfile -Force
+    }
+
+    add-content -path $outfile -value '<RegistryEvent onmatch="include">' 
+    $keys = $keys | Sort-Object
+    foreach ($key in $keys) {
+        add-content -path $outfile -value ("`t" + '<TargetObject condition="contains">' + $key + '</TargetObject>')
+    }
+    add-content -path $outfile -value '</RegistryEvent>'
+
+    #open the file in notepad
+    start-Process -FilePath ($env:windir + '\notepad.exe') -ArgumentList $outfile
 }
+
