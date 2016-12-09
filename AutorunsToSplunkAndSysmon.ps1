dbcc85b9b449e0fef901e33490e4b04fbcca6038
dstaulcu <David_Staulcup@hotmail.com>
2016-12-08 21:31:03 -0500

    added support for file/folder aseps

--- AutorunsToSplunkAndSysmon.ps1
+++ AutorunsToSplunkAndSysmon.ps1
@@ -12,6 +12,9 @@ $outfile_sysmon = ($env:TEMP + '\sysmon_autoruns_rules.xml')
 $outfile_splunk = ($env:TEMP + '\splunk_autoruns_inputs.conf')
 $input_lines = Get-Content $input_file
 
+$outfile_sysmon | Remove-Item | Out-Null
+$outfile_splunk | Remove-Item | Out-Null
+
 $asep_locations = [System.Collections.ArrayList]@()
 #transform the input
 foreach ($line in $input_lines) {
@@ -32,11 +35,13 @@ foreach ($line in $input_lines) {
 $asep_locations = $asep_locations | Sort-Object
 
 $Keys = [System.Collections.ArrayList]@()
+$Files = [System.Collections.ArrayList]@()
+$Folders = [System.Collections.ArrayList]@()
 
 if ($asep_locations) {
 
     foreach ($entry in $asep_locations) {
-        # reduce list to only include registry type entries
+        # identify registry type entries
         if ($entry -like 'HKLM*' -or $entry -like 'HKCU*') {
 
             # strip off hklm\hkcu prefix to keys
@@ -49,19 +54,40 @@ if ($asep_locations) {
                 $keys.Add($key) | Out-Null
             }
         }
+
+        # identify file system entries
+        if ($entry.Substring(1,2) -eq ':\') {
+
+            # handle files vs. directories
+            if ($entry.Substring($entry.Length - 4,1) -eq '.') {
+                if ($Files.Contains($Files) -eq $false) {
+                    $Files.Add($entry) | Out-Null
+                }
+            }
+            else {
+                if ($Folders.Contains($Folders) -eq $false) {
+                    $Folders.Add($entry) | Out-Null
+                }
+
+            }
+
+        }
     }
 
     if (Get-Item($outfile_sysmon) | Out-Null) {
         Remove-Item -path $outfile_sysmon -Force | Out-Null
     } 
 
+
     if (Get-Item($outfile_splunk) | Out-Null) {
         Remove-Item -path $outfile_splunk -Force | Out-Null
     }
 
     $keys = $keys | Sort-Object
+    $Files = $Files | Sort-Object
+    $Folders = $Folders | Sort-Object
 
-    # output to Splunk
+    # output regkeys to Splunk
     $item = 0
     foreach ($key in $keys) {
         $item++
@@ -73,12 +99,31 @@ if ($asep_locations) {
         add-content -path $outfile_splunk -value ("proc = .*")
         add-content -path $outfile_splunk -value ("type = set|create|delete|rename")
         add-content -path $outfile_splunk -value ("baseline = 0")
-        add-content -path $outfile_splunk -value ("index = main")
-    
+        add-content -path $outfile_splunk -value ("index = main")    
     }
 
-    # output to sysmon
+    # output files to Splunk
+    $item = 0
+    foreach ($file in $files) {
+        $item++
+        $file = $file -replace '\\Users\\\w+\\','\Users\*\'
+        add-content -path $outfile_splunk -value ('')
+        add-content -path $outfile_splunk -value ('[monitor://' + $file + ']')
+        add-content -path $outfile_splunk -value ("index = main")
+    }
 
+    # output folders to Splunk
+    $item = 0
+    foreach ($folder in $folders) {
+        $item++
+        $folder = $folder -replace '\\Users\\\w+\\','\Users\*\'
+        add-content -path $outfile_splunk -value ('')
+        add-content -path $outfile_splunk -value ('[monitor://' + $folder + ']')
+        add-content -path $outfile_splunk -value ("index = main")
+    }
+
+
+    # output regkeys to sysmon
     add-content -path $outfile_sysmon -value '<RegistryEvent onmatch="include">' 
     $keys = $keys | Sort-Object
     foreach ($key in $keys) {
@@ -86,9 +131,31 @@ if ($asep_locations) {
     }
     add-content -path $outfile_sysmon -value '</RegistryEvent>'
 
-    #open the file in notepad
+    # output files to sysmon
+    $item = 0
+    add-content -path $outfile_sysmon -value '<FileCreate onmatch="include">' 
+
+    foreach ($file in $files) {
+        $item++
+        $file = $file -replace '\w:\\Users\\\w+',''
+        add-content -path $outfile_sysmon -value ("`t" + '<TargetFilename condition="contains">' + $file + '</TargetFilename>')
+    }
+
+    foreach ($folder in $folders) {
+        $item++
+        $folder = $folder -replace '\w:\\Users\\\w+',''
+        add-content -path $outfile_sysmon -value ("`t" + '<TargetFilename condition="contains">' + $folder + '</TargetFilename>')
+    }
+
+
+    add-content -path $outfile_sysmon -value '</FileCreate>'
+
+
+    #open the files in notepad
     start-Process -FilePath ($env:windir + '\notepad.exe') -ArgumentList $outfile_sysmon
     start-Process -FilePath ($env:windir + '\notepad.exe') -ArgumentList $outfile_splunk
 }
 
-write-host ([string]$keys.Count + ' registry-based asep mointoring rules created.')
+write-host ([string]$keys.Count + ' registry-based asep monitoring rules created.')
+write-host ([string]$folders.Count + ' folder-based asep monitoring rules created.')
+write-host ([string]$files.Count + ' folder-based asep monitoring rules created.')
